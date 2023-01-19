51927dafb0c52e71f022921efd41764d56b26338
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-01-19 22:31:23 +0000

    [feat]: Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Pagination
+"""
+import requests
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    a function named index_range that
+    takes two integer arguments page 
+    and page_size and returns a tuple
+    """
+    pass
