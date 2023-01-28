c16f033c34ae0985c3b490789363c3c787a5bff5
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-01-28 00:19:21 +0000

    [update]: fifo caching system

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,14 +15,22 @@ class FIFOCache(BaseCaching):
         initializes the cache
         """
         super().__init__()
-        self.cache_data = {}
-    
+        self.cache_data_list = []
+
     def put(self, key, item):
         """
 
         """
         if key and item:
             self.cache_data[key] = item
+            self.cache_data_list.append(key)
+        if len(self.cache_data_list) > BaseCaching.MAX_ITEMS:
+            self.cache_data.pop(self.cache_data_list[0])
+            discard = self.cache_data_list.pop(0)
+            print("DISCARD: {}".format(discard))
 
-        else:
-            pass
+    def get(self, key):
+        """ get a key from the cache """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
