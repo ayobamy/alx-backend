dbd9eb6573b2f45605ece7d013070f5d6cdaad62
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-28 13:43:26 +0000

    [feat]: connecting redis to local server

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,9 +1,10 @@
+import redis from 'redis';
 import { createClient } from 'redis';
 
 const client = redis.createClient();
 
 client.on('error', (err) => {
-  console.log(`Redis client not connected to the server: ${err}`);
+  console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
 client.on('connect', () => {
